184dd13745e7a2a1db84ca58d04e57e93f124d5b
wow-woo <51289355+wow-woo@users.noreply.github.com>
2023-09-24 17:09:38 +0900

    togglable sFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     var body: some View {
         HStack {
             CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
         }
         .foregroundStyle(.orange)
         .padding()
@@ -21,22 +21,26 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp:Bool = false
+    @State var isFaceUp = false
 
     var body: some View {
-        ZStack(content: {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
+                base
                     .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
+                base
                     .strokeBorder(lineWidth: 2)
                 Text("🐱")
                     .font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 12)
+                base
+                    .fill() // fill is default.
             }
-            
+        }
+        .onTapGesture (count:1, perform:{
+            isFaceUp.toggle()
         })
     }
 }
